fde6fef732b02495cd7d57a7b50812192ab52bc7
Jakub Kołuda <jko01@outlook.com>
2022-11-10 19:38:06 +0100

    Add MEalsList, change styles.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Components/Header/Header";
 import MealsInfo from "./Components/Meals/MealsInfo";
+import MealsList from "./Components/Meals/MealsList";
 function App() {
   return (
     <React.Fragment>
       <Header />
       <main>
         <MealsInfo />
+        <MealsList />
       </main>
     </React.Fragment>
   );
---
+++ src/Components/Meals/MealsList.js
@@ -0,0 +1,10 @@
+import classes from './MealsList.module.scss'
+const MealsList = () => {
+    return (
+        <ul className={classes.list}>
+            
+        </ul>
+    )
+    }
+
+export default MealsList
